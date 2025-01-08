2097834de098c5d01b44222e0a7da6d6367a5256
slh-git <shsieh603@gmail.com>
2025-01-08 15:12:36 -0500

    Pass WindowThread Obj into the CreateThread
    Pass WindowThread Obj into the CreateThread method using void pointers and lpParm

--- main.cpp
+++ main.cpp
@@ -3,56 +3,16 @@
 #include "toml++/toml.hpp"
 #include "vk_codes.h"
 #include <thread>
+#include <vector>
 //using namespace Windows::Foundation;
-//static bool isWindowHidden = true;
-//static HWND wezHwnd;
+// Function declaration
+DWORD WINAPI WindowThreadFunction(LPVOID lpParam);
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
 
+// Global variables
 DWORD tlsIndex;
-//// The callback function for the WH_KEYBOARD_LL hook
-//LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
-//	//The wParam and lParam parameters contain information about a keyboard message. (https://learn.microsoft.com/en-us/windows/win32/winmsg/lowlevelkeyboardproc)	
-//	if (nCode == HC_ACTION) {
-//		PKBDLLHOOKSTRUCT keypress = (PKBDLLHOOKSTRUCT)lParam;
-//		// Get the dimension of the monitor
-//		DimensionStruct monitorDim = GetMonitorDimension(wezHwnd);
-//
-//		// Get the dimension of the window based on the percentage of the monitor
-//		WindowDimensionStruct winDim = GetWinDimensionByPercent(monitorDim, 100, 50);
-//
-//		switch (wParam) {
-//			// The WM_KEYDOWN message is posted to the window with the keyboard focus when a nonsystem key is pressed. A nonsystem key is a key that is pressed when the ALT key is not pressed.
-//		case WM_KEYDOWN:
-//			if (keypress->vkCode == vkCode) {
-//				wcout << L"F1 Pressed" << endl;
-//
-//				if (isWindowHidden) {
-//					// Set the window position and 
-//					SetWindowPos(wezHwnd, HWND_TOPMOST, 0, 0, winDim.width, winDim.height, SWP_SHOWWINDOW);
-//					isWindowHidden = false;
-//				}
-//				else {
-//					// Hide the window
-//					SetWindowPos(wezHwnd, HWND_TOPMOST, 0, 0, winDim.width, winDim.height, SWP_HIDEWINDOW);
-//					isWindowHidden = true;
-//				}
-//
-//			}
-//			if (wezHwnd == nullptr) {
-//				wcerr << L"Error: Window not found" << endl;
-//				return 1;
-//			}
-//
-//			break;
-//		case WM_SYSKEYDOWN:
-//			break;
-//		case WM_KEYUP:
-//			break;
-//		case WM_SYSKEYUP:
-//			break;
-//		}
-//	}
-//	return CallNextHookEx(NULL, nCode, wParam, lParam);
-//}
+// The callback function for the WH_KEYBOARD_LL hook
+
 
 toml::table ParseToml(string filename) {
 	toml::table table;
@@ -87,73 +47,128 @@ toml::table ParseToml(string filename, int index) {
 }
 
 
+
 // Use a struct to store the wezHwnd and the WindowDimensionStruct for multi-threading, add vkcode too
 struct WindowThread {
 	HWND hwnd;
 	WindowDimensionStruct winDim;
 	int vkCode;
 	HANDLE hThread;
-
+	int TLSIndex;
 	WindowThread() {
 		this->hwnd = nullptr;
 		this->winDim = { 0,0,0,0 };
 		this->vkCode = -1;
 	}
 
-
-	DWORD WINAPI WindowThreadFunction(LPVOID lpParam) {
-		if (this->hwnd == nullptr) {
-			wcerr << L"Error: Window not found" << endl;
-			return 1;
-		}
-
-		////WH_KEYBOARD_LL hook
-		HHOOK hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, NULL, 0);
-
-		// Keep this app running until we're told to stop
-		MSG msg;
-		while (!GetMessage(&msg, NULL, NULL, NULL)) {    //this while loop keeps the hook
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-
-		UnhookWindowsHookEx(hHook);
-	}
-	WindowThread(LPCWSTR className, WindowDimensionStruct winDim, string keyName) {
+	WindowThread(LPCWSTR className, WindowDimensionStruct winDim, string keyName, int tlsIndex) {
 		this->hwnd = SearchWindow(L"org.wezfurlong.wezterm", NULL);;
 		this->winDim = winDim;
 		this->vkCode = VKCodes::getVKCode(keyName);
 		this->hThread = CreateThread(NULL, 0, WindowThreadFunction, this, 0, NULL);
+		if (!hThread)
+		{
+			printf("Failed to create hthread. Error: %lu\n", GetLastError());
+		}
+		this->TLSIndex = tlsIndex;
+
 	}
 };
+DWORD WINAPI WindowThreadFunction(LPVOID lpParam) {
+	WindowThread* windowThread = (WindowThread*)lpParam;
+	if (windowThread->hwnd == nullptr) {
+		wcerr << L"Error: Window not found" << endl;
+		return 1;
+	}
+	printf("windowThread tlsindex= %d\n", windowThread->TLSIndex);
 
 
+	//WH_KEYBOARD_LL hook
+	HHOOK hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, NULL, 0);
+
+	// Keep this app running until we're told to stop
+	MSG msg;
+	while (!GetMessage(&msg, NULL, NULL, NULL)) {    //this while loop keeps the hook
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	UnhookWindowsHookEx(hHook);
+	return 1;
+}
+
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+	//	//The wParam and lParam parameters contain information about a keyboard message. (https://learn.microsoft.com/en-us/windows/win32/winmsg/lowlevelkeyboardproc)	
+	//	if (nCode == HC_ACTION) {
+	//		PKBDLLHOOKSTRUCT keypress = (PKBDLLHOOKSTRUCT)lParam;
+	//		// Get the dimension of the monitor
+	//		DimensionStruct monitorDim = GetMonitorDimension(wezHwnd);
+	//
+	//		// Get the dimension of the window based on the percentage of the monitor
+	//		WindowDimensionStruct winDim = GetWinDimensionByPercent(monitorDim, 100, 50);
+	//
+	//		switch (wParam) {
+	//			// The WM_KEYDOWN message is posted to the window with the keyboard focus when a nonsystem key is pressed. A nonsystem key is a key that is pressed when the ALT key is not pressed.
+	//		case WM_KEYDOWN:
+	//			if (keypress->vkCode == vkCode) {
+	//				wcout << L"F1 Pressed" << endl;
+	//
+	//				if (isWindowHidden) {
+	//					// Set the window position and 
+	//					SetWindowPos(wezHwnd, HWND_TOPMOST, 0, 0, winDim.width, winDim.height, SWP_SHOWWINDOW);
+	//					isWindowHidden = false;
+	//				}
+	//				else {
+	//					// Hide the window
+	//					SetWindowPos(wezHwnd, HWND_TOPMOST, 0, 0, winDim.width, winDim.height, SWP_HIDEWINDOW);
+	//					isWindowHidden = true;
+	//				}
+	//
+	//			}
+	//			if (wezHwnd == nullptr) {
+	//				wcerr << L"Error: Window not found" << endl;
+	//				return 1;
+	//			}
+	//
+	//			break;
+	//		case WM_SYSKEYDOWN:
+	//			break;
+	//		case WM_KEYUP:
+	//			break;
+	//		case WM_SYSKEYUP:
+	//			break;
+	//		}
+	//	}
+		return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
 
 int main() {
 	//PrintWindows(true);
 	//PrintDesktopWindows(true);
 
 	toml::table tomlTable = ParseToml("config.toml");
-	WindowThread* scratchpadThreads = new WindowThread[tomlTable.size()];
+	vector<WindowThread> scratchpadThreads;
+	scratchpadThreads.reserve(tomlTable.size()); // Reserve space for efficiency
 
 	std::cout << tomlTable << "\n";
-	int i = 0;
+
 	for (const auto& [tableName, subNode] : tomlTable) {
 		std::cout << tableName << "\n";
 
 		auto subTable = subNode.as_table();
 		WindowDimensionStruct winDim = {
-			(int) subTable->at("width").as_integer()->get(),
-			(int) subTable->at("height").as_integer()->get()
+			static_cast<int>(subTable->at("width").as_integer()->get()),
+			static_cast<int>(subTable->at("height").as_integer()->get())
 		};
-		scratchpadThreads[i] = WindowThread(
+		scratchpadThreads.emplace_back(
 			(LPCWSTR)subTable->at("classname").as_string(),
 			winDim,
-			subTable->get_as<string>("key")->get()
+			subTable->get_as<std::string>("key")->get(),
+			scratchpadThreads.size()
 		);
-		
-			i++;
 	}
+
+
 	std::cout << tomlTable["Wezterm"]["classname"] << "\n";
 	/*wezHwnd = SearchWindow(L"org.wezfurlong.wezterm", NULL);*/
 	
@@ -173,7 +188,6 @@ int main() {
 		/*std::cout << "Window " << i << ":\n";
 		scratchpadThreads[i].winDim.Print();
 		std::cout << "VK Code: " << scratchpadThreads[i].vkCode << "\n";*/
-		HANDLE hThread = CreateThread(NULL, 0, WindowThreadFunction, &scratchpadThreads[i], 0, NULL);
 	}
 
 
